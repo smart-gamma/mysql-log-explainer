e6d4fe8c67d53d09a69f5c9eeceaf262caf7657d
Evgen <jekccs@gmail.com>
2019-03-31 18:19:59 +0300

    pdo connected

--- src/SmartGamma/MySqlExplainer/Service/AnalyzerProvider/MySqlDuration.php
+++ src/SmartGamma/MySqlExplainer/Service/AnalyzerProvider/MySqlDuration.php
@@ -4,8 +4,28 @@ namespace SmartGamma\MySqlExplainer\Service\AnalyzerProvider;
 
 class MySqlDuration implements AnalyzerProviderInterface
 {
+    /**
+     * @var \PDO
+     */
+    private $connection;
+
+    public function __construct()
+    {
+        $this->connection = new \PDO(
+            'mysql:host=mysql;dbname=3w1AdminDB',
+            'root',
+            'notsecretpass'
+        );
+    }
+
     public function execute(string $query): string
     {
-        return 'duration result';
+        $this->connection->query('set profiling=1');
+        $this->connection->query($query);
+        $res = $this->connection->query('show profiles');
+        $records = $res->fetchAll(\PDO::FETCH_ASSOC);
+        $duration = $records[0]['Duration'];
+
+        return $duration;
     }
 }
--- src/SmartGamma/MySqlExplainer/Service/AnalyzerProvider/MySqlExplain.php
+++ src/SmartGamma/MySqlExplainer/Service/AnalyzerProvider/MySqlExplain.php
@@ -4,8 +4,26 @@ namespace SmartGamma\MySqlExplainer\Service\AnalyzerProvider;
 
 class MySqlExplain implements AnalyzerProviderInterface
 {
+    /**
+     * @var \PDO
+     */
+    private $connection;
+
+    public function __construct()
+    {
+        $this->connection = new \PDO(
+            'mysql:host=mysql;dbname=3w1AdminDB',
+            'root',
+            'notsecretpass'
+        );
+    }
+
     public function execute(string $query): string
     {
-        return 'explain result';
+        $res = $this->connection->query('EXPLAIN '.$query);
+        $records = $res->fetchAll(\PDO::FETCH_ASSOC);
+        var_dump($records);
+
+        return $records;
     }
 }
